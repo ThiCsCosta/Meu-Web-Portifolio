1ba179cf041d0228dfd3a8143f9554c5404d2f40
ThiCsCosta <thycscosta@gmail.com>
2024-07-30 18:53:50 -0300

    Adicionado script no modal

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
-
+const modal = document.getElementById("modal-sobre");
+const abrirModal = document.getElementById("modal_");
+const closeModal = document.getElementById("btn-close");
 /**********************CONFIG MENU BURGUER**********************/
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
@@ -19,6 +21,21 @@ navLinks.addEventListener("click", (e) => {
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 })
 
+
+/****************************************************************************************/
+
+/*Eventos Modal*/
+
+abrirModal.addEventListener("click", ()=>{
+    modal.style.display = "flex"
+});
+
+modal.addEventListener("click", (event) =>{
+    if(event.target === modal){
+        modal.style.display = "none"
+    }
+});
+closeModal.addEventListener("click", () => modal.style.display = "none");
 
 /****************************************************************************************/
 const bookingType = document.getElementById("booking-type");
